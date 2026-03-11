2ec009c5a57d8eb401e17da7606ae56364102ca7
Salsabeel Khaled <salsabeel1053.003s@gmail.com>
2026-03-11 17:13:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("btn").addEventListener("click", () => {
+  const msg = document.getElementById("msg");
+  msg.innerText = "The JavaScript logic is working! Pipeline test: OK.";
+  msg.style.color = "#4ecca3";
+});
